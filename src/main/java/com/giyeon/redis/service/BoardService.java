0455683b35c4177d087afc2722ffc969b4bfe483
GiyeonChoi <bdcgy22@daum.net>
2025-01-09 17:20:54 +0900

    feat : 기본 api 생성 & dummy data 10000 Insert

---
+++ src/main/java/com/giyeon/redis/service/BoardService.java
@@ -0,0 +1,27 @@
+package com.giyeon.redis.service;
+
+
+import com.giyeon.redis.domain.Board;
+import com.giyeon.redis.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+    private BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public List<Board> getBoards(int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
+        return pageOfBoards.getContent();
+    }
+}
+
