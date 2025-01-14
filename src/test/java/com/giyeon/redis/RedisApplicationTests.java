cd90ab824ee881cd15696186fe41f6dad7476d5d
GiyeonChoi <bdcgy22@daum.net>
2025-01-14 16:31:35 +0900

    Revert "first commit"
    This reverts commit 1a3e286cc7e2dc04d52bb9f5600a71ccacb761af.

--- src/test/java/com/giyeon/redis/RedisApplicationTests.java
+++ src/test/java/com/giyeon/redis/RedisApplicationTests.java
@@ -3,8 +3,10 @@ package com.giyeon.redis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class RedisApplicationTests {
 
+    @Test
     void contextLoads() {
     }
 
