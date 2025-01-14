1a3e286cc7e2dc04d52bb9f5600a71ccacb761af
GiyeonChoi <bdcgy22@daum.net>
2025-01-14 16:28:47 +0900

    first commit

--- src/test/java/com/giyeon/redis/RedisApplicationTests.java
+++ src/test/java/com/giyeon/redis/RedisApplicationTests.java
@@ -3,10 +3,8 @@ package com.giyeon.redis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class RedisApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
